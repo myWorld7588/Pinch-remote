6e73e8a887208f35da3ff3e302f26f0b7e964b03
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-17 13:37:08 -0500

    # Double Tap Gesture
    ### Create a new property to store the actual value of the image scale
    
    `@State private var imageScale: CGFloat = 1`
    
    **Add a new Gesture view modifier to the image**

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,7 +11,12 @@ struct ContentView: View {
     
     // MARK: - PROPERTY
     
+    // This mutable variable will store the actual state of the animation and trigger whenever
     @State private var isAnimating: Bool = false
+    
+    // Default value of imagescale is 1
+    // Each time when users double tap on the image
+    @State private var imageScale: CGFloat = 1
     
     // MARK: - BODY
     
@@ -25,11 +30,30 @@ struct ContentView: View {
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-                            
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
                 
+                // MARK: - 1. Tap Gesture
+                    .onTapGesture(count: 2, perform: {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    })
             } //: ZSTACK
             .navigationTitle("Pinch & Zoon")
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear(perform: {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            })
+            
         } //: NAVIGATION
     }
 }
@@ -39,5 +63,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
