f0cc87fd7386d534b3c57876f7a77beb3c132d47
myWorld7588 <jin.jakechoi@gmail.com>
2022-12-16 21:13:48 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Jake Choi on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
